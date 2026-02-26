5d2ae2df0d861c5b66d507fa0a61f8cc845b14b5
Appu_architect <appugouda.architect@gmail.com>
2026-02-26 14:49:01 +0530

    test: remove module apigateway from config + fix

--- infra/platform/terraform/environments/dev/outputs.tf
+++ infra/platform/terraform/environments/dev/outputs.tf
@@ -2,6 +2,6 @@ output "lambda_function_name" {
   value = module.lambda.lambda_name
 }
 
-output "api_endpoint" {
-  value = module.apigateway.api_endpoint
-}
+# output "api_endpoint" {
+#   value = module.apigateway.api_endpoint
+# }
